a6fd3384d88946f2f3e9c4918baa3250ea00fa56
benjammin12 <benjammin12@users.noreply.github.com>
2015-09-11 08:46:06 -0400

    Update HelloWorld.java
    Basic simple average program in java8

--- New Project/HelloWorld.java
+++ New Project/HelloWorld.java
@@ -1,22 +1,26 @@
-import java.util.Scanner;
+// Author: Ben Xerri
+// Objective:  Prompt user for 10 numbers, then Average.
+ 
+ 
+ import java.util.Scanner;
 
 public class HelloWorld{
 
      public static void main(String []args){
         Scanner user_input = new Scanner(System.in);
         
-        int x = 0;
-        int total = 0;
+        int x = 0; //used to count 
+        int total = 0; //total
         
         while (x < 10) {
             
-            System.out.println("Enter a number: ");
+            System.out.println("Enter a number: "); //prompt for number
             
-            int number = user_input.nextInt();
+            int number = user_input.nextInt(); 
            
             total = total + number;
             
-            System.out.println("Total equals: " + total);
+            System.out.println("Total equals: " + total); //updates total
             
             x++;
             
@@ -24,9 +28,9 @@ public class HelloWorld{
             
         }
         
-        int average = total / x;
+        int average = total / x;  //divide total by amount counted in x
         
-        System.out.println("Your average is: " + average);
+        System.out.println("Your average is: " + average); // output average
         
      }
 }
