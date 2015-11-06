bbdc03a4894110fe84e32068c0970e8dc234fc2c
benjammin12 <benjammin12@users.noreply.github.com>
2015-11-06 13:51:05 -0500

    Create inchesToMilimeters.java

---
+++ inchesToMilimeters.java
@@ -0,0 +1,32 @@
+//author: Ben 
+//objective: convert inches to meters
+
+/*
+Psuedocode:
+millimeters = multiply 1 inch by 25.4 
+get user_input for amount of inches
+multiply user amount by millimeters and re-sign in new milimeters
+print out milimeteres
+*/
+
+import java.util.Scanner;
+
+public class inchesToMilimeters{
+
+     public static void main(String []args){
+         
+         double miliMeters = (1* 25.4); // 1 inch is 25.4 milimeters
+         
+            Scanner user_input = new Scanner(System.in);
+         
+            System.out.print("Enter the inches you would like to convert to milimeters: ");
+         
+                double inches = user_input.nextDouble();
+         
+                 miliMeters = inches * miliMeters;
+         
+         System.out.println("Your inches is " + miliMeters + " milimeters.");
+         
+        
+     }
+}
